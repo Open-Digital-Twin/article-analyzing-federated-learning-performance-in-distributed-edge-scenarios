6d25ea492d90da62e683a782b24c0ff7cad22a23
remde <ffremde@gmail.com>
2022-03-03 14:22:40 -0300

    Fix async for loop

--- observability/src/observer.js
+++ observability/src/observer.js
@@ -3,9 +3,9 @@ const dockerHandler = require('./docker-handler.js')
 observe = async () => {
     const nodes = ['ffremde01', 'ffremde02', 'ffremde03', 'ffremde-master'];
     const containerInfos = await dockerHandler.getAllContainerInfo(nodes);
-    containerInfos.forEach(containerInfo => {
-        containerInfo.stats = dockerHandler.getContainerStats(containerInfo.host, containerInfo.ID);
-    });
+    for (let containerInfo of containerInfos) {
+        containerInfo.stats = await dockerHandler.getContainerStats(containerInfo.host, containerInfo.ID);
+    }
     console.log(containerInfos);
 }
 
